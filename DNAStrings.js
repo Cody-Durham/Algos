38bcd4f3e13962c917511add8fe679851acee4aa
codydurham <codydd@gmail.com>
2021-08-23 13:54:52 -0600

    Solved / Practice

--- DNAStrings.js
+++ DNAStrings.js
@@ -18,10 +18,10 @@
 // QUICK NOTES: 
 // A - T compliments (A replaced by T) A => T
 // T - A compliments (T replaced by A) T => A
-// G - C compliments (G replaced by C) G => G
+// G - C compliments (G replaced by C) G => C
 // C - G compliments (C replaced by G) C => G
 
-// YOUR PSEUDOCODE GOES HERE
+// PSEUDOCODE GOES HERE
 // declare new empty string
 // for loop over 'string'
   // if string[i] === 'A' => 'T'
@@ -30,22 +30,53 @@
   // if string[i] === 'C' => 'G'
 // Return newString
 
-  let dnaString = 'ATTGC' // output should be 'TAACG'
-  
-  const dnaTransform = string => {
-      // declaring a new variable here to start stacking in letters 
-      let newString = ""
-      for (let i = 0; i < string.length; i++) {
-          if (string[i] === 'A') {
-              newString += 'T'
-          } if (string[i] === 'T') {
-              newString += 'A'
-          } if (string[i] === 'G') {
-              newString += 'C'
-          } if (string[i] === 'C') {
-              newString += 'G'
-          }
-        }
-        return newString
+
+/////////////////////////////////////////////////////////////////////////////
+// practice   8-23-21
+/////////////////////////////////////////////////////////////////////////////
+const transform = (string) => {
+  let empty = ''
+  for (let i = 0; i < string.length; i++) {
+    // console.log(string[i]);
+    if (string[i] === 'A') {
+      empty += 'T'
+    }
+    if (string[i] === 'T') {
+      empty += 'A'
+    }
+    if (string[i] === 'G') {
+      empty += 'C'
+    }
+    if (string[i] === 'C') {
+      empty += 'G'
+    }
+  }
+  return empty
+
 }
-  console.log(dnaTransform(dnaString))
+console.log(transform('ATTCG'))
+
+
+
+
+
+
+//   let dnaString = 'ATTGC' // output should be 'TAACG'
+  
+//   const dnaTransform = string => {
+//       // declaring a new variable here to start stacking in letters 
+//       let newString = ""
+//       for (let i = 0; i < string.length; i++) {
+//           if (string[i] === 'A') {
+//               newString += 'T'
+//           } if (string[i] === 'T') {
+//               newString += 'A'
+//           } if (string[i] === 'G') {
+//               newString += 'C'
+//           } if (string[i] === 'C') {
+//               newString += 'G'
+//           }
+//         }
+//         return newString
+// }
+//   console.log(dnaTransform(dnaString))
